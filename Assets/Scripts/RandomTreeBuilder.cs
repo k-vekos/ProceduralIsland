7398aa9a91cc4f0574753cd39167be1ed4160472
k-vekos <kvekos@gmail.com>
2020-02-29 13:37:09 -0800

    Refactored random tree code to separate rendering part out

---
+++ Assets/Scripts/RandomTreeBuilder.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public static class RandomTreeBuilder
+{
+    public static Tree GetRandomTree(int iterations, float growthLength)
+    {
+        // Place first node randomly in area and add to tree
+        var root = RandomSample(0);
+        var tree = new Tree(root);
+        
+        for (var i = 1; i < iterations; i++)
+        {
+            GrowTree(tree, i, growthLength);
+        }
+        
+        return tree;
+    }
+    
+    private static void GrowTree(Tree tree, int iteration, float growthLength)
+    {
+        // Get a random sample
+        var sample = RandomSample(iteration);
+        
+        // Get the closest node in the tree to the sample
+        var closest = tree.GetClosestNode(sample);
+
+        // Create a new node between the closest node and the sample.
+        var extension = ExtendToward(closest, sample, growthLength, iteration);
+        
+        // If we managed to create a new node, add it to the tree.
+        if (extension != null)
+        {
+            tree.AddChild(closest, extension);
+        }
+        
+        // Debug.Log($"Finished creating tree with {tree.Nodes.Count} nodes in {iteration} iterations.");
+    }
+
+    private static TreeNode RandomSample(int nodeIndex)
+    {
+        return new TreeNode(Random.Range(0f, 1f), Random.Range(0f, 1f), nodeIndex);
+    }
+
+    private static TreeNode ExtendToward(TreeNode from, TreeNode towards, float growthLength, int iteration)
+    {
+        var dir = towards.position - from.position;
+        var dist = dir.magnitude;
+        dir = dir.normalized;
+        dir = from.position + (dir * Mathf.Min(growthLength, dist));
+        return new TreeNode(dir.x, dir.y, iteration, from);
+    }
+}
---
+++ Assets/Scripts/TreeRenderer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class TreeRenderer : MonoBehaviour
+{
+    public int iterations = 3;
+    public float size = 100f;
+    public GameObject nodeObject;
+    public LineRenderer lineRendererObject;
+    public float growthLength = 0.1f;
+
+    private Tree _tree;
+
+    void Start()
+    {
+        _tree = RandomTreeBuilder.GetRandomTree(iterations, growthLength);
+        SpawnNodesAndLines();
+    }
+
+    void SpawnNodesAndLines()
+    {
+        for (var i = 0; i < _tree.Nodes.Count; i++)
+        {
+            var treeNode = _tree.Nodes[i];
+            var newNodeObject = Instantiate(nodeObject, GetNodeWorldPosition(treeNode), Quaternion.identity, transform);
+            newNodeObject.name = "Node_" + treeNode.nodeIndex;
+            
+            if (treeNode.parent != null)
+                AddLineRenderer(treeNode, newNodeObject);
+        }
+    }
+    
+    private void AddLineRenderer(TreeNode node, GameObject nodeGameObject)
+    {
+        // Add line renderer
+        var lineRenderer = Instantiate(lineRendererObject, Vector3.zero, Quaternion.identity, nodeGameObject.transform);
+                
+        // Add line point at parent position
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPositions(new[]
+        {
+            GetNodeWorldPosition(node.parent),
+            GetNodeWorldPosition(node)
+        });
+                
+        // Color line according to iteration #
+        var color = Color.Lerp(Color.green, Color.red, (float) node.nodeIndex / iterations);
+        lineRenderer.startColor = color;
+        lineRenderer.endColor = color;
+    }
+    
+    private Vector3 GetNodeWorldPosition(TreeNode treeNode)
+    {
+        return transform.TransformPoint(new Vector3(treeNode.position.x, 0f, treeNode.position.y) * size);
+    }
+}
